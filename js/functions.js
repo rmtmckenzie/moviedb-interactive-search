1be9922fae6a3efd573d1e0da8036c98aa151293
Morgan McKenzie <rmtm@uvic.ca>
2014-08-27 00:58:53 -0700

    Add states and 'constants' to key entry
    States keep track of whether currently waiting, loading, done, or empty.
    Allows bar to move up and down.
    
    Working on #4.

--- js/functions.js
+++ js/functions.js
@@ -10,33 +10,81 @@
 
 // remap jQuery to $
 (function($){
+	var STATES = Object.freeze({EMPTY:0,UNLOADED:1,LOADING:2,LOADED:3});
 
-	var curTimeout = -1;
+
+	var typeTimeout = -1;
+	var loadTimeout = -1;
 	var averageRespTime = 100;
+	var state = STATES.EMPTY;
+	
+	var KEYTIMEOUT = 1000;
 
 	/* trigger when page is ready */
 	$(document).ready(function (){
+		var searchDiv = $(".search")
 		var searchBar = $(".search .search-bar");
 		var searchProgress = $(".search .search-progress");
 		
-		function performSearch(){
-			curTimeout = -1;
-			textVal = searchBar.val();
-			searchProgress.addClass("right");
-			searchProgress.animate({width:0},1000);
-			//theMovieDb.search.getMulti({query:textVal},function(a){console.log(a)},function(b){console.log(b)}));
+		function doneSearch(){
+			state = STATES.LOADED;
+			searchDiv.addClass("top");
+			//searchDiv.animate({top:0},1000)
+		}
+		
+		function keyTimeout(){
+		
+			if (searchBar.val().length == 0){
+				//clear all
+				searchDiv.removeClass("top");
+				state = STATES.EMPTY;
+				//todo: delete masonry blocks
+				$(window).resize();
+			} else {
+			
+				state = STATES.LOADING;
+				typeTimeout = -1;
+				textVal = searchBar.val();
+				searchProgress.addClass("right");
+				searchProgress.animate({width:0},1000);
+				
+				loadTimeout = setTimeout(doneSearch, 1000);
+				
+				//theMovieDb.search.getMulti({query:textVal},function(a){console.log(a)},function(b){console.log(b)}));
+			}
 		}
 		
 		searchBar.bind('input propertychange', function(d,e,f) {
-			clearTimeout(curTimeout);
-			curTimeout = setTimeout(performSearch, 1000);
-			searchProgress.clearQueue();
-			searchProgress.animate({width:0},2);
-			searchProgress.removeClass("right");
-			searchProgress.animate({width:searchProgress.parent().width()},1000);
+			clearTimeout(typeTimeout);
+			clearTimeout(doneSearch);
+			var l = searchBar.val().length;
+			
+			if (0 < l && l < 3) {
+				//don't erase last results or show new
+				if(state != STATES.LOADED){
+					state = STATES.UNLOADED
+				}
+			} else {
+				//3 or greater, wait for next keypress or timeout
+				typeTimeout = setTimeout(keyTimeout, KEYTIMEOUT);
+				searchProgress.clearQueue();
+				searchProgress.animate({width:0},2);
+				searchProgress.removeClass("right");
+				searchProgress.animate({width:searchProgress.parent().width()},KEYTIMEOUT);
+			}
 		});
 	});
 	
+	
+	$(window).resize(function (){
+		if(state == STATES.EMPTY){
+			console.log("resizing", state)
+			$('.search').css({
+				top: ($(window).height() - $('.search').outerHeight())/2
+			});
+		}
+    });
+    $(window).resize();
 	
 	/* optional triggers
 	
